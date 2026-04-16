64a6d41a02368cce93d53900ef6341c0dbb38ece
zhuman <384535415@qq.com>
2026-04-16 14:51:26 +0800

    refactor: point production update check to GitHub Releases API

--- src/renderer/services/endpoints.ts
+++ src/renderer/services/endpoints.ts
@@ -9,14 +9,18 @@ const isTestMode = () => {
   return configService.getConfig().app?.testMode === true;
 };
 
+// GitHub (从 git remote 确认: https://github.com/menhulu233/gearlai)
+const GITHUB_OWNER = 'menhulu233';
+const GITHUB_REPO = 'gearlai';
+
 // 自动更新
 export const getUpdateCheckUrl = () => isTestMode()
   ? 'http://localhost:18000/openapi/get/luna/hardware/gearlai/test/update'
-  : 'http://localhost:18000/openapi/get/luna/hardware/gearlai/prod/update';
+  : `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest`;
 
 export const getFallbackDownloadUrl = () => isTestMode()
   ? 'http://localhost:18000/#/download-list'
-  : 'http://localhost:18000/#/download-list';
+  : `https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest`;
 
 // Skill 商店
 export const getSkillStoreUrl = () => isTestMode()
